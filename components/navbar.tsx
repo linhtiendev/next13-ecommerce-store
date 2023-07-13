0ed52ed0fc1a9f8835b513fce35d17294e44c72a
linhtiendev <lelinhtien0707@gmail.com>
2023-07-13 17:46:12 +0700

    Individual product screen (Store)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
-import Container from "@/components/ui/container";
 import Link from "next/link";
 
+import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
@@ -15,7 +15,7 @@ const Navbar = async () => {
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                        <p className="font-bold text-xl">Store</p>
+                        <p className="font-bold text-2xl">LITI STORE</p>
                     </Link>
                     <MainNav
                         data={categories}
