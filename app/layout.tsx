87618048fe4ab2d9bdc158b45d84c0763f1cb1de
linhtiendev <lelinhtien0707@gmail.com>
2023-07-22 01:08:27 +0700

    Add to Cart functionality (Store)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
 import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -24,6 +25,9 @@ export default function RootLayout({
       <body 
         className={font.className}>
           <ModalProvider />
+          <ToastProvider>
+
+          </ToastProvider>
           <Navbar />
           {children}
           <Footer />
--- hooks/use-preview-modal.ts
+++ hooks/use-preview-modal.ts
@@ -11,7 +11,7 @@ interface PreviewModalStore {
 const usePreviewModal = create<PreviewModalStore>((set) => ({
     isOpen: false,
     data: undefined,
-    onOpen: (data: Product) => set({ data: data, isOpen: true }),
+    onOpen: (data: Product) => set({ data, isOpen: true }),
     onClose: () => set({ isOpen: false})
 }));
 
